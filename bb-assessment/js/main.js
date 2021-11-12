b02687b1fc857b4b032622ba05746de9a62e5ef5
mjatta <30508772+mjatta@users.noreply.github.com>
2021-11-12 18:14:23 -0500

    Final Version

---
+++ bb-assessment/js/main.js
@@ -0,0 +1,56 @@
+/*------------------------Slide show functionality --------------------------*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+/*-------------------Logic for Slideshow--------------------------*/ 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+ 
+}
+
+/*----------------Mobile Toggle----------------*/
+
+function mobileToggle() {
+    var x = document.getElementById("mobile-navbar");
+    console.log(x, 'nav')
+    if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+  }
+
+/*--------------Accordion-------------------*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
